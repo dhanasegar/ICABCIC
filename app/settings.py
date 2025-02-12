47ac438255444fbcb01bc373367da5e237ba1c7f
dhanasegar <dhanasegarbit@gmail.com>
2025-02-12 20:54:49 +0530

    error msg1

--- app/settings.py
+++ app/settings.py
@@ -152,5 +152,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL=False                         
-EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_USER = 't@gmail.com'
 EMAIL_HOST_PASSWORD = ''
