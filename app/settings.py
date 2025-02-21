8e278e1a4d27af6c38de9b49b589a2cbb16b4c43
dhanasegar <dhanasegarbit@gmail.com>
2025-02-22 02:10:31 +0530

    first commit

--- app/settings.py
+++ app/settings.py
@@ -152,5 +152,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL=False                         
-EMAIL_HOST_USER = 'dhanasegarbit@gmail.com'
-EMAIL_HOST_PASSWORD = 'mapoifktknqesujx'
+EMAIL_HOST_USER = 'bit@gmail.com'
+EMAIL_HOST_PASSWORD = 'm'
